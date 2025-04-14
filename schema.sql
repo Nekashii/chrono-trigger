3ff2bb9f5549c09d77f0a521e989010c9dae7fff
Rebecca Alitzel <nekashii@outlook.com>
2025-04-13 19:48:28 -0600

    fix: change text type in database schema

--- schema.sql
+++ schema.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS events;
 
 CREATE TABLE
     IF NOT EXISTS events (
-        id STRING PRIMARY KEY,
+        id TEXT PRIMARY KEY,
         first_trigger_at INTEGER,
         next_trigger_at INTEGER,
         trigger_every INTEGER,
-        webhook_url STRING,
-        webhook_method STRING,
-        webhook_params STRING,
-        webhook_headers STRING,
-        webhook_body STRING
+        webhook_url TEXT,
+        webhook_method TEXT,
+        webhook_params TEXT,
+        webhook_headers TEXT,
+        webhook_body TEXT
     );
